4ae9b2d0d5b3e680e7ff576828440bb171ee31bb
Harinder Singh <hrinder.maan@gmail.com>
2022-08-08 22:15:57 -0400

    Code Cleaned

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,6 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
-import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("");
